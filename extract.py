3aaa867243e22e42d9f6aa2394c3724903ff5951
mflood <hermannwest@gmail.com>
2024-06-23 11:39:03 -0600

    Update formatting of extract.py

--- extract.py
+++ extract.py
@@ -14,9 +14,10 @@ You'll edit this file in Task 2.
 """
 import csv
 import json
-
-from models import NearEarthObject, CloseApproach
 from typing import List
+
+from models import CloseApproach, NearEarthObject
+
 
 def load_neos(neo_csv_path: str) -> List[NearEarthObject]:
     """Read near-Earth object information from a CSV file.
@@ -24,16 +25,16 @@ def load_neos(neo_csv_path: str) -> List[NearEarthObject]:
     :param neo_csv_path: A path to a CSV file containing data about near-Earth objects.
     :return: A collection of `NearEarthObject`s.
     """
-    with open (neo_csv_path, "r", encoding='utf-8') as handle:
+    with open(neo_csv_path, "r", encoding="utf-8") as handle:
         dict_reader = csv.DictReader(handle)
         neo_list = []
         for item in dict_reader:
             neo = NearEarthObject(
-                designation=item['pdes'],
-                name=item['name'],
-                diameter=item['diameter'],
-                hazardous=item['pha'],
-        )
+                designation=item["pdes"],
+                name=item["name"],
+                diameter=item["diameter"],
+                hazardous=item["pha"],
+            )
             neo_list.append(neo)
     return neo_list
 
@@ -45,27 +46,25 @@ def load_approaches(cad_json_path):
     :return: A collection of `CloseApproach`es.
     """
     # TODO: Load close approach data from the given JSON file.
-        
+
     approach_list = []
     with open(cad_json_path, "r", encoding="utf-8") as handle:
         cad_object = json.load(handle)
-        fields = cad_object['fields']
-        data = cad_object['data']
+        fields = cad_object["fields"]
+        data = cad_object["data"]
         for item in data:
             item_dict = dict(zip(fields, item))
-            # {'des': '170903', 'orbit_id': '105', 'jd': '2415020.507669610', 'cd': '1900-Jan-01 00:11', 
-            # 'dist': '0.0921795123769547', 'dist_min': '0.0912006569517418', 'dist_max': '0.0931589328621254', 
+            # {'des': '170903', 'orbit_id': '105', 'jd': '2415020.507669610', 'cd': '1900-Jan-01 00:11',
+            # 'dist': '0.0921795123769547', 'dist_min': '0.0912006569517418', 'dist_max': '0.0931589328621254',
             # 'v_rel': '16.7523040362574', 'v_inf': '16.7505784933163', 't_sigma_f': '01:00', 'h': '18.1'}
             close_approach = CloseApproach(
-                designation = item_dict['des'],
-                time = item_dict['cd'],
-                distance = item_dict['dist'],
-                velocity = item_dict['v_rel'],
+                designation=item_dict["des"],
+                time=item_dict["cd"],
+                distance=item_dict["dist"],
+                velocity=item_dict["v_rel"],
             )
             approach_list.append(close_approach)
     return approach_list
-
-
 
 
 if __name__ == "__main__":
